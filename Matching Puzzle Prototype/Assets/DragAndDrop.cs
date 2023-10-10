940e369f076f7c51a2c433db25d16c2769faa47f
cathartictea <cathartictea@gmail.com>
2023-10-10 12:16:09 -0500

    Basic script for prototype

---
+++ Matching Puzzle Prototype/Assets/DragAndDrop.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Provides funtionality for handling and managing UI events;
+//Used for creativng interactive UI elements and handling user input
+//Event Systems --------- manages user input and dispatches events to UI elements; Tracks input deices like mouse, keyboard, or touch and forward eveents to the appropriate UI elements
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
